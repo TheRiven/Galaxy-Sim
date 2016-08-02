f00fa66180dba7c1a4d236274e7edb10bf139971
TheRiven <Novajames276@gmail.com>
2016-08-02 15:46:06 +0100

    Created InputController
    Created the InputController object and class.
    Moved the Camera control code into the new class.

--- Scripts/Controllers/GameController.cs
+++ Scripts/Controllers/GameController.cs
@@ -31,9 +31,6 @@ public class GameController : MonoBehaviour {
 
     Galaxy theGalaxy;
 
-    Vector3 currFramePostion;
-    Vector3 lastFramePostion;
-
     #endregion ----------------
 
     // Use this for initialization
@@ -51,16 +48,7 @@ public class GameController : MonoBehaviour {
     void Update ()
     {
                 
-        currFramePostion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetMouseButton(1) || Input.GetMouseButton(2) )
-        {
-            CameraScrolling();
-        }
-
-        CameraZooming();
-
-        lastFramePostion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
 
     }
 
@@ -98,22 +86,6 @@ public class GameController : MonoBehaviour {
     }
 
 
-    void CameraScrolling() // Handle screen dragging
-    {
-        // FIXME: needs to set a limit to how far you can move the camera so that you don't get lost.
-
-        //Debug.Log ("Current Mouse Position: " + currFramePostion);
-        Vector3 diff = lastFramePostion - currFramePostion;
-        Camera.main.transform.Translate(diff);
-
-    }
-
-
-    void CameraZooming() // Allows the scroll wheel to alter the camera zoom level
-    {
-        Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
-        // Restricts the zoom level to within size 3 to 25
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 5f, 25f);
-    }
+    
 
 }
